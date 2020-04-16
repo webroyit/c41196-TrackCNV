71d7ba0c7a576f69d7cb87cc40892c6b2a6bde3b
royweb <xytank@gmail.com>
2020-04-16 13:27:36 -0400

    Add index.js on components folder to import all the files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Component } from 'react';
 
+import { Cards, Chart, CountryPicker } from './components';
+
 class App extends Component{
   render(){
     return(
       <div>
-        <h1>App</h1>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
     )
   }
