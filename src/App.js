df6e7b3560ef736e9470a7329512687d9381e1af
royweb <xytank@gmail.com>
2020-04-16 15:06:20 -0400

    Add a get request to the API

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, {Component } from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+// no need to write out index.js when using that file name
+import { fetchData } from './api';
 
 class App extends Component{
+  // put async key first before method name because it a life cycle methods
+  async componentDidMount(){
+    const data = await fetchData();
+
+    console.log(data);
+  }
   render(){
     return(
       <div className={styles.container}>
