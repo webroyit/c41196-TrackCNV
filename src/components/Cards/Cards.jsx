3db870a49293b7f9825253726e4533d32cdba49a
royweb <xytank@gmail.com>
2020-04-17 12:11:27 -0400

    Use Card, CardContent, and Typography to create a card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
+import styles from './Cards.module.css';
 const Cards = (props) => {
     console.log(props)
+    // gutterBottom for padding bottom
     return(
-        <div>
-            <Grid container spacing={3} justify="center">
-                
+        <div className={styles.container}>
+          <Grid container spacing={3} justify="center">
+            <Grid item component={Card}>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <Typography varaint="h5">Data</Typography>
+                <Typography color="textSecondary">Date</Typography>
+                <Typography varaint="body2">Number of active cases of COVID-19</Typography>
+              </CardContent>
             </Grid>
+            <Grid item component={Card}>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                <Typography varaint="h5">Data</Typography>
+                <Typography color="textSecondary">Date</Typography>
+                <Typography varaint="body2">Number of recoveries from COVID-19</Typography>
+              </CardContent>
+            </Grid>
+            <Grid item component={Card}>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                <Typography varaint="h5">Data</Typography>
+                <Typography color="textSecondary">Date</Typography>
+                <Typography varaint="body2">Number of deaths caused by COVID-19</Typography>
+              </CardContent>
+            </Grid>
+          </Grid>
         </div>
     )
 }
