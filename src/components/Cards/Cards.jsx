c74df8e649f64d5694093b623866e295f4aff776
royweb <xytank@gmail.com>
2020-04-18 13:05:24 -0400

    Use react-countup to add animation on the number

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
 import styles from './Cards.module.css';
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
     if(!confirmed){
       return "Loading...";
     }
-    // gutterBottom for padding bottom
+    // "gutterBottom" for padding bottom
     return(
         <div className={styles.container}>
           <Grid container spacing={3} justify="center">
             <Grid item component={Card}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                <Typography varaint="h5">{confirmed.value}</Typography>
+                <Typography varaint="h5">
+                  <CountUp
+                    start={0}
+                    end={confirmed.value}
+                    duration={3}
+                    separator="," />
+                </Typography>
                 <Typography color="textSecondary">{lastUpdate}</Typography>
                 <Typography varaint="body2">Number of active cases of COVID-19</Typography>
               </CardContent>
@@ -21,7 +28,13 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
             <Grid item component={Card}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                <Typography varaint="h5">{recovered.value}</Typography>
+                <Typography varaint="h5">
+                  <CountUp
+                    start={0}
+                    end={recovered.value}
+                    duration={3}
+                    separator="," />
+                </Typography>
                 <Typography color="textSecondary">{lastUpdate}</Typography>
                 <Typography varaint="body2">Number of recoveries from COVID-19</Typography>
               </CardContent>
@@ -29,7 +42,13 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
             <Grid item component={Card}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                <Typography varaint="h5">{deaths.value}</Typography>
+                <Typography varaint="h5">
+                  <CountUp
+                    start={0}
+                    end={deaths.value}
+                    duration={3}
+                    separator="," />
+                </Typography>
                 <Typography color="textSecondary">{lastUpdate}</Typography>
                 <Typography varaint="body2">Number of deaths caused by COVID-19</Typography>
               </CardContent>
