3ed0b578010fb907ec7a6183452905c0a8b9e51f
royweb <xytank@gmail.com>
2020-04-18 13:42:36 -0400

    Use classnames to apply more than 1 styles when using module css

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 import styles from './Cards.module.css';
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
@@ -11,7 +12,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
     return(
         <div className={styles.container}>
           <Grid container spacing={3} justify="center">
-            <Grid item component={Card} xs={12} md={3}>
+            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)} >
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>Infected</Typography>
                 <Typography varaint="h5">
@@ -25,7 +26,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                 <Typography varaint="body2">Number of active cases of COVID-19</Typography>
               </CardContent>
             </Grid>
-            <Grid item component={Card} xs={12} md={3}>
+            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)} >
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                 <Typography varaint="h5">
@@ -39,7 +40,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                 <Typography varaint="body2">Number of recoveries from COVID-19</Typography>
               </CardContent>
             </Grid>
-            <Grid item component={Card} xs={12} md={3}>
+            <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)} >
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                 <Typography varaint="h5">
